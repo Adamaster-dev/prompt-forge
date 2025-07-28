3c35ed6fa7beaed2358e228b6dc4dd6dabe24e14
Adamaster-dev <aberete8556@gmail.com>
2025-07-28 14:12:36 +0200

    Fix AppState definition in env.ollama.js

--- env.ollama.js
+++ env.ollama.js
@@ -1,7 +1,7 @@
-const ENV = {
+const AppState = {
     PORT: 8080,
     DATABASE_PATH: './api/promptforge.db',
     DEFAULT_AI_PROVIDER: 'ollama',
     OLLAMA_BASE_URL: 'http://localhost:11434',
-    API_BASE_URL: 'http://localhost:8080/api'
+    API_BASE: 'http://localhost:8080/api'
 };
